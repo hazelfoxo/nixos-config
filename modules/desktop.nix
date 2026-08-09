e93f289ef29c9a535e5a7195a5432589b190f7ba
Hazie <nicksu2005@gmail.com>
2026-08-09 20:17:39 +0100

    Remove redundant config

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -7,12 +7,6 @@
   # Enable KDE Plasma
   services.desktopManager.plasma6.enable = true;
 
-  # Enable Firefox Serivce
-  programs.firefox.enable = true;
-
-  # Enable Steam Service
-  programs.steam.enable = true;
-
   # Install KDE Specific Packages
   environment.systemPackages = with pkgs; [
     kdePackages.sddm-kcm
