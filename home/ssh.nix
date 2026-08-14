085a97117b2b46ec6f938ea895361de15aac8ba6
Hazie <nicksu2005@gmail.com>
2026-08-14 14:41:30 +0100

    Implement new .settings configuration

--- home/ssh.nix
+++ home/ssh.nix
@@ -6,6 +6,7 @@
     enable = true;
     # Disable default SSH config to prepare for removal of defaults
     enableDefaultConfig = false;
+    settings."*" = {};
     # Automatically use github ssh keys for repo access
     extraConfig = ''
       Host github.com
