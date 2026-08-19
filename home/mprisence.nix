55c88c7f84876dddba258a0dfd178af01115a502
Hazie <nicksu2005@gmail.com>
2026-08-19 19:43:58 +0100

    Comment on mprisence module

--- home/mprisence.nix
+++ home/mprisence.nix
@@ -1,10 +1,12 @@
 { pkgs, ... }:
 
 {
+  # Include the mprisense cmd tool
   home.packages = [
     pkgs.mprisence
   ];
 
+  # Create a config to enable youtube detection
   home.file.".config/mprisence/config.toml".text = ''
     [web_player.youtube]
     ignore = false
