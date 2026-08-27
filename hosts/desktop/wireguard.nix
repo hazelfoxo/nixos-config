f668750f97a452346978f2310f122a0a01bcac87
Hazie <nicksu2005@gmail.com>
2026-08-27 23:39:04 +0100

    setup desktop wireguard config

---
+++ hosts/desktop/wireguard.nix
@@ -0,0 +1,24 @@
+{ ... }:
+
+{
+  my.wireguard = {
+    enable = true;
+    
+    connectionName = "HomeVPN";
+
+    address = [
+      "10.0.0.4/32"
+    ];
+
+    serverPublicKey =
+      "SY57GcLr+gbxxN278xyepRXm37ZbLikM5+b4pRp+anQ=";
+
+    endpoint = "hazie.duckdns.org:51820";
+
+    allowedIPs = [
+      "10.0.0.1/32"
+    ];
+
+    persistentKeepalive = 25;
+  };
+}
