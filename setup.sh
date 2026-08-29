3fca2deabd042a3613d16b34a20f23deae3f798d
Hazie <nicksu2005@gmail.com>
2026-08-29 17:03:05 +0100

    Tidy up output of setup.sh

--- setup.sh
+++ setup.sh
@@ -11,29 +11,37 @@ read -rp "Host: " HOST
 DEVICE_SECRET="$REPO_DIR/secrets/device-keys/$HOST.txt"
 
 [[ -f "$KEY" ]] || {
-echo "Missing personal key: $KEY"
+echo "Error: Personal key not found."
 exit 1
 }
 
 [[ -f "$DEVICE_SECRET" ]] || {
-echo "Missing device secret: $DEVICE_SECRET"
+echo "Error: Device secret for '$HOST' not found."
 exit 1
 }
 
 export SOPS_AGE_KEY_FILE="$KEY"
 
-echo "Using key: $SOPS_AGE_KEY_FILE"
+echo
+echo "==> Linking NixOS configuration..."
 
 sudo rm -rf "$ETC_NIXOS"
 sudo ln -s "$REPO_DIR" "$ETC_NIXOS"
+
+echo "==> Installing device key..."
 
 sudo install -d -m 700 /var/lib/sops-nix
 
 sops decrypt "$DEVICE_SECRET" |
 sudo install -m 600 /dev/stdin /var/lib/sops-nix/key.txt
 
+echo "==> Rebuilding NixOS..."
+
 sudo nixos-rebuild switch --flake "$ETC_NIXOS#$HOST"
+
+echo "==> Removing temporary personal key..."
 
 rm -f "$KEY"
 
+echo
 echo "Done."
