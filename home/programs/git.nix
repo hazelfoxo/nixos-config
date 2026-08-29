d582f31646e9129a37f1f8c828c08c5e3bf2521e
Hazie <nicksu2005@gmail.com>
2026-08-29 19:30:16 +0100

    Restructure credentials in git config

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -7,8 +7,10 @@
 
     settings ={
     # Define default git credentials
-    user.name = "Hazie";
-    user.email = "nicksu2005@gmail.com";
+    user = {
+      name = "Hazie";
+      email = "nicksu2005@gmail.com";
+    };
     # Define default git branch name
     init.defaultBranch = "main";
     };
