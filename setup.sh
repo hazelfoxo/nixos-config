3aeca5e98e9df41969878d735ff5cb5ddb27e858
Hazie <nicksu2005@gmail.com>
2026-08-29 16:33:54 +0100

    fix path

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ KEY="$REPO_DIR/keys.txt"
 
 read -rp "Host: " HOST
 
-DEVICE_SECRET="$REPO_DIR/secrets/device-keys/$HOST.yaml"
+DEVICE_SECRET="$REPO_DIR/secrets/$HOST.yaml"
 
 [[ -f "$KEY" ]] || {
 echo "Missing personal key: $KEY"
