436b9fbc2096781c2ce3ffe03d43fd86663d058e
Hazie <nicksu2005@gmail.com>
2026-09-01 07:50:59 +0100

    Make the nm-file-secret-agent run in user mode

--- modules/networking/networking.nix
+++ modules/networking/networking.nix
@@ -3,4 +3,10 @@
 {
   # Enable Network Manager Service
   networking.networkmanager.enable = true;
+
+  # Make the nm-file-secret-agent run in user mode
+  systemd.services.nm-file-secret-agent.serviceConfig = {
+    User = "hazie";
+    Group = "users";
+  };
 }
