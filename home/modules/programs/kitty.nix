787999c414f52bef71fb0b10e661b062e0f698fd
Hazie <nicksu2005@gmail.com>
2026-09-02 03:31:58 +0100

    Fix formatting in kitty.nix

--- home/modules/programs/kitty.nix
+++ home/modules/programs/kitty.nix
@@ -1,62 +1,62 @@
-{  ... }:
+{ ... }:
 
 {
-programs.kitty = {
-  enable = true;
+  programs.kitty = {
+    enable = true;
 
-   font = {
-    name = "CaskaydiaCove Nerd Font";
+    font = {
+      name = "CaskaydiaCove Nerd Font";
+    };
+
+    settings = {
+
+      remember_window_size = "no";
+      initial_window_width = "120c";
+      initial_window_height = "35c";
+
+      background = "#000000";
+      foreground = "#ffffff";
+
+      cursor = "#f7c8e0";
+      cursor_text_color = "#000000";
+
+      selection_background = "#302632";
+      selection_foreground = "#fff7fc";
+
+      url_color = "#b9dcf5";
+
+      color0 = "#000000";
+      color8 = "#655c6b";
+
+      color1 = "#e9a8bd";
+      color9 = "#f3b9cc";
+
+      color2 = "#a8d8c0";
+      color10 = "#bce8d1";
+
+      color3 = "#efd0a6";
+      color11 = "#f6dbb5";
+
+      color4 = "#a9c8e8";
+      color12 = "#bad7f2";
+
+      color5 = "#c6afd9";
+      color13 = "#d8c0eb";
+
+      color6 = "#a7d5d5";
+      color14 = "#b9e5e4";
+
+      color7 = "#d0d0d0";
+      color15 = "#ffffff";
+
+      tab_bar_background = "#000000";
+      active_tab_background = "#d8c0eb";
+      active_tab_foreground = "#000000";
+      inactive_tab_background = "#151117";
+      inactive_tab_foreground = "#aaa0b0";
+
+      active_border_color = "#d8c0eb";
+      inactive_border_color = "#29232c";
+    };
   };
-
-  settings = {
-
-    remember_window_size = "no";
-    initial_window_width = "120c";
-    initial_window_height = "35c";
-
-    background = "#000000";
-    foreground = "#ffffff";
-
-    cursor = "#f7c8e0";
-    cursor_text_color = "#000000";
-
-    selection_background = "#302632";
-    selection_foreground = "#fff7fc";
-
-    url_color = "#b9dcf5";
-
-    color0 = "#000000";
-    color8 = "#655c6b";
-
-    color1 = "#e9a8bd";
-    color9 = "#f3b9cc";
-
-    color2 = "#a8d8c0";
-    color10 = "#bce8d1";
-
-    color3 = "#efd0a6";
-    color11 = "#f6dbb5";
-
-    color4 = "#a9c8e8";
-    color12 = "#bad7f2";
-
-    color5 = "#c6afd9";
-    color13 = "#d8c0eb";
-
-    color6 = "#a7d5d5";
-    color14 = "#b9e5e4";
-
-    color7 = "#d0d0d0";
-    color15 = "#ffffff";
-
-    tab_bar_background = "#000000";
-    active_tab_background = "#d8c0eb";
-    active_tab_foreground = "#000000";
-    inactive_tab_background = "#151117";
-    inactive_tab_foreground = "#aaa0b0";
-
-    active_border_color = "#d8c0eb";
-    inactive_border_color = "#29232c";
-  };
-};
 }
