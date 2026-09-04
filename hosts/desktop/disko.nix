260fc3d4b9437caa53f388d2517dac37c028c0f4
Hazie <nicksu2005@gmail.com>
2026-09-04 22:42:47 +0100

    Rename root file system on dikso to NixOS

--- hosts/desktop/disko.nix
+++ hosts/desktop/disko.nix
@@ -41,6 +41,7 @@
               type = "filesystem";
               format = "ext4";
               mountpoint = "/";
+              extraArgs = [ "-L" "NixOS" ];
             };
           };
         };
