4645ba44bb437c50a1445683ea2a40f8a62d486c
Hazie <nicksu2005@gmail.com>
2026-09-07 16:17:24 +0000

    fixed disko config for laptop

--- hosts/laptop/disko.nix
+++ hosts/laptop/disko.nix
@@ -18,10 +18,7 @@
               type = "filesystem";
               format = "vfat";
               mountpoint = "/boot";
-              mountOptions = [
-                "fmask=0077"
-                "dmask=0077"
-              ];
+              mountOptions = [ "umask=0077" ];
             };
           };
 
