4552d4f56fe36d4b644144326acdfdc7b32d6aeb
Hazie <nicksu2005@gmail.com>
2026-09-07 20:46:07 +0100

    Make lockscreen wallpaper sync with desktop

--- home/modules/desktop/kde.nix
+++ home/modules/desktop/kde.nix
@@ -5,7 +5,8 @@
     enable = osConfig.my.profiles.desktop.kde.enable;
 
     workspace.wallpaper = osConfig.my.profiles.desktop.kde.wallpaper;
-
+    kscreenlocker.wallpaper = osConfig.my.profiles.desktop.kde.wallpaper;
+    
     input = {
       touchpads = [
         {
