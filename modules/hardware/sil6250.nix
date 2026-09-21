3e039955234d04dd3b32a68f3b0d5bc187d7f6c1
Hazie <nicksu2005@gmail.com>
2026-09-21 21:39:44 +0100

    Disable polkit and sudo support for fprint

--- modules/hardware/sil6250.nix
+++ modules/hardware/sil6250.nix
@@ -159,11 +159,12 @@ in
     # block password login, see NixOS/nixpkgs#239770); nixpkgs creates a separate
     # `kde-fingerprint` service instead, which we replicate here.
     security.pam.services = {
-      login.fprintAuth = true;
+      # sddm.fprintAuth = true;
+      # login.fprintAuth = true;
       kde-fingerprint.fprintAuth = true;
-      kde-fingerprint.p11Auth = false;
-      polkit-1.fprintAuth = true;
-      sudo.fprintAuth = true;
+      # kde-fingerprint.p11Auth = false;
+      # polkit-1.fprintAuth = true;
+      # sudo.fprintAuth = true;
     };
   };
 }
