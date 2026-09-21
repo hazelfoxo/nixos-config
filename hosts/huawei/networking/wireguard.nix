a292669909a2e7cda2c291cfe5a61a0319c9b966
Hazie <nicksu2005@gmail.com>
2026-09-21 07:16:35 +0100

    Add wireguard creds to huawei

--- hosts/huawei/networking/wireguard.nix
+++ hosts/huawei/networking/wireguard.nix
@@ -6,7 +6,7 @@
   my.wireguard.profiles."Home VPN" = {
     privateKeySecret = "homeserver-wg-private-key";
     interface = "wg0";
-    address = [ "10.0.0.2/32" ];
+    address = [ "10.0.0.5/32" ];
     serverPublicKey = "SY57GcLr+gbxxN278xyepRXm37ZbLikM5+b4pRp+anQ=";
     endpoint = "hazie.duckdns.org:51820";
     allowedIPs = [ "10.0.0.1/32" ];
