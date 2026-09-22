f77b529e82c93e1312f26c17fb9fcf3c6997a2c5
Hazie <nicksu2005@gmail.com>
2026-09-22 19:57:26 +0100

    Add more shared networks

--- modules/profiles/shared-networking.nix
+++ modules/profiles/shared-networking.nix
@@ -13,9 +13,34 @@ in
   config = lib.mkIf cfg.wifi.enable {
     my.wifi = {
       enable = true;
-      connectionName = "Home Wi-Fi";
-      ssidSecret = "home-wifi-ssid";
-      passwordSecret = "home-wifi-password";
+
+      profiles."Home Wi-Fi" = {
+        connectionName = "Home Wi-Fi";
+        ssidSecret = "home-wifi-ssid";
+        passwordSecret = "home-wifi-password";
+      };
+
+      profiles."Phone Hotspot" = {
+        connectionName = "Phone Hotspot";
+        ssidSecret = "phone-hotspot-ssid";
+        passwordSecret = "phone-hotspot-password";
+      };
+
+      # More connections go here.
+      #
+      # Example: WPA-Enterprise (university / eduroam Wi-Fi)
+      #
+      # profiles."School Wifi" = {
+      #   connectionName = "School Wifi";
+      #   ssidSecret = "school-wifi-ssid";
+      #   eap = {
+      #     enable = true;
+      #     method = "peap";
+      #     identitySecret = "school-wifi-identity";
+      #     passwordSecret = "school-wifi-password";
+      #     phase2Auth = "mschapv2";
+      #   };
+      # };
     };
   };
 }
